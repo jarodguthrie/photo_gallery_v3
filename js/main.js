c9b40c641e06c924f89b2a1db9305d3271dd984d
Jarod Guthrie <jarod.guthrie@gmail.com>
2017-06-26 16:19:32 +1000

    finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-$('#search-highlight').hideseek({
-  highlight: true
-});
 
+function searchInput() {
+  $('#search').on('keyup', function() {
+    var searchData = $(this).val();
+    $('li a').each(function() {
+      var textSearch = $(this).attr('data-title');
+      if( textSearch.search(new RegExp(searchData, 'i')) < 0) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+      var listQty = $('.hide').children().length;
+      var hiddenImgages = $('li').children(':hidden').length;
+      if(hiddenImgages === listQty) {
+        $('.no-img').html("<h1>No images found</h1>");
+      }
+      if(hiddenImgages < listQty) {
+        $('h1').empty();
+      }
+    });
+  });
+}
+
+searchInput();
